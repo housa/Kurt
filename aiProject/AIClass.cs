e3899ac66b5e303e3b47c050e4d518add1cc39ad
housa <alexandra.hou@gmail.com>
2017-03-11 16:45:48 +0100

    initial

---
+++ aiProject/AIClass.cs
@@ -0,0 +1,63 @@
+﻿ using System;
+using Torque3D;
+using Torque3D.Engine.Util.Enums;
+using Torque3D.Util;
+
+namespace GameAI
+{
+   public class AIClass
+   {
+       private static bool inPursuit;
+       private static bool tookDamage;
+       private static bool playerInRange;
+
+       [ConsoleFunction]
+       public static PlayerAction Kurt(FeatureVector vector)
+       {
+           tookDamage = vector.TicksSinceDamage < 15;
+           inPursuit = vector.DamageProb > 0.0;
+           playerInRange = vector.DamageProb > 0.8;
+
+           if (inPursuit)
+               return PursueOpponent(vector);
+           if (tookDamage)
+               return DodgeOpponent(vector);
+           return SearchForOpponent(vector);
+       }
+
+       private static PlayerAction SearchForOpponent(FeatureVector vector)
+       {
+           return PlayerAction.TurnLeft;
+       }
+
+       private static PlayerAction PursueOpponent(FeatureVector vector)
+       {
+           if (playerInRange && vector.ShootDelay == 0)
+               return PlayerAction.Shoot;
+
+           if (vector.DeltaDamageProb < 0.0)
+           {
+               if (vector.DeltaRot > 0.0)
+               {
+                   return PlayerAction.TurnLeft;
+               }
+               return PlayerAction.TurnRight;
+           }
+           else
+           {
+               if (vector.DeltaRot > 0.0)
+               {
+                   return PlayerAction.TurnRight;
+               }
+               return PlayerAction.TurnLeft;
+           }
+
+           return PlayerAction.MoveForward;
+       }
+
+       private static PlayerAction DodgeOpponent(FeatureVector vector)
+       {
+           return PlayerAction.MoveForward;
+       }
+   }
+}
